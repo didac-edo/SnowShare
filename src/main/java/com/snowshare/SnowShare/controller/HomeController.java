e0276c2bd4239b3b85ef3c66392f756d0399c2fe
Didac-Edo <80513402+Didac-Edo@users.noreply.github.com>
2023-04-11 17:48:14 +0200

    commit master

--- src/main/java/com/snowshare/SnowShare/controller/HomeController.java
+++ src/main/java/com/snowshare/SnowShare/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    //kike
+
     @GetMapping("/")
     public String index() {
         return "index";
